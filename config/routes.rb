c008a95779e4940e4588325aacd4990ce82cde61
shibani <shibani@8thlight.com>
2018-04-27 16:30:01 -0400

    adding site controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'site/home'
   root to: 'site#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
