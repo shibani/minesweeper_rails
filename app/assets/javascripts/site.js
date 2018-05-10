c9df90379b91b15186ae4cc3f970fe50ffcf23a5
shibani <shibani@8thlight.com>
2018-05-10 14:24:05 -0400

    adding check for right-click

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+window.onload = function(){
+  document.querySelector(".form-container").onmousedown = function (e) {
+    console.log(e.which);
+    switch (e.which) {
+      case 1:
+        console.log('Left');
+        //submit form here
+        break;
+      case 3:
+        console.log('Right');
+        //also submit form here but with flag params
+        break;
+    }
+  }
+}
