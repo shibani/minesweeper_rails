d3403e2f9b8e415bfc98ffc68fadd689df5463ed
shibani <shibani@8thlight.com>
2018-05-30 13:31:10 -0400

    updating gem and switching branch

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -70,13 +70,15 @@ RSpec.describe SiteController, type: :controller do
 
     params3 = { 'content': 'B', 'index': '0', 'rowsize'=>'4', 'positions': 'B,1, , ,0,0,1,2,B,1,2,2,0,0,0,0', 'revealed': '0'}
 
-    params4 = { 'content': 'F', 'index': '1', 'rowsize'=>'4', 'positions': 'BF,1,X,X,0,0,1,2,B,1,2,2,0,0,0,0'}
+    params4 = { 'content': 'F', 'index': '1', 'rowsize'=>'4', 'positions': 'BF,1, , ,0,0,1,2,B,1,2,2,0,0,0,0'}
 
-    params5 = { 'content': 'F', 'index': '1', 'rowsize'=>'5', 'positions': 'BF,B, , , , , , ,BF, , , , , , , , , ,BF, , , , ,BF, ', 'revealed': '1'}
+    params5 = { 'content': 'F', 'index': '1', 'rowsize'=>'5', 'positions': 'B,B, , , , , , ,B, , , , , , , , , ,B, , , , ,B, ', 'revealed': '1'}
 
     params6 = { 'content': 'F', 'index': '1', 'rowsize'=>'5', 'positions': 'B,B, , , , , , ,B, , , , , , , , , ,B, , , , ,B, ', 'revealed': '2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,19,20,21,22,24', 'flags': '0,8,18,23'}
 
-    params7 = { 'content': 'F', 'index': '1', 'rowsize'=>'5', 'flags': '1,2,3', 'positions': 'B,B, , , , , , ,BF, , , , , , , , , ,BF, , , , ,BF, ', 'revealed': '10,11,12'}
+    params7 = { 'content': 'F', 'index': '1', 'rowsize'=>'5', 'flags': '1,2,3', 'positions': 'B,B, , , , , , ,B, , , , , , , , , ,B, , , , ,B, ', 'revealed': '10,11,12'}
+
+    params8 = { 'content': 'F', 'index': '1', 'rowsize'=>'5', 'flags': '0,1,8,18,23', 'positions': 'B,B, , , , , , ,B, , , , , , , , , ,B, , , , ,B, ', 'revealed': '10,11,12'}
 
     let (:game) {SiteController.create_game(5,5)}
 
